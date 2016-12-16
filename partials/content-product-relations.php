54ba9104342f0c97bf2e91950a6d5166a2d1c01f
Kristian Strehlau <krischi.s@web.de>
2016-12-16 21:04:56 +0100

    Added missing Product info and button and thumbnail effects

--- partials/content-product-relations.php
+++ partials/content-product-relations.php
@@ -25,6 +25,9 @@
         </ul>
 
         <div class="tab-content">
+            <div>
+                <h4 class="lead"><b>10</b> ähnliche Produkte gefunden: </h4>
+            </div>
             <?php if(!empty($relatedProductsQuery)): ?>
                 <div class="tab-pane fade in active" id="related-products">
                 <?php if(!empty($relatedProductsQuery) && $relatedProductsQuery->have_posts()): ?>
@@ -32,8 +35,8 @@
                         <?php while($relatedProductsQuery->have_posts()): $relatedProductsQuery->the_post(); ?>
                         <?php $affiliateLink = aff_get_product_affiliate_link($relatedProductsQuery->post); ?>
 
-                            <div class="col-md-4 col-sm-6">
-                                <div class="related-products-tumbnail">
+                            <div class="col-md-6">
+                                <div class="thumbnail">
 
                                     <?php if(has_post_thumbnail()): ?>
                                         <?php $linkPreviewImage = afft_link_product_preview_image(); ?>
@@ -56,15 +59,22 @@
                                             <p>
                                                 <a href="<?php echo $affiliateLink; ?>" class="btn btn-buy center-block"
                                                    role="button" rel="nofollow" target="_blank">
+                                                    <i class="fa fa-shopping-cart fa-lg"></i>
                                                     <?php _e('Buy', 'affilicious-theme'); ?>
                                                 </a>
                                             </p>
                                         <?php endif; ?>
                                         <p>
                                             <a href="<?php the_permalink() ?>" class="btn btn-review center-block" role="button">
+                                                <i class="fa fa-book fa-lg"></i>
                                                 <?php _e('To The Test Report', 'affilicious-theme'); ?>
                                             </a>
                                         </p>
+                                        <div class="caption-shop-info">
+                                            <p class="text-muted">Ink. 19% MwSt.</p>
+                                            <p class="text-muted">Kostenloseser Versand</p>
+                                            <p class="text-muted">Zuletzt Aktualisiert am 23.12.2016</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -82,7 +92,7 @@
                         <?php while($relatedAccessoriesQuery->have_posts()): $relatedAccessoriesQuery->the_post(); ?>
                             <?php $affiliateLink = aff_get_product_link($relatedAccessoriesQuery->post); ?>
 
-                            <div class="col-md-4">
+                            <div class="col-md-6">
                                 <div class="thumbnail">
 
                                     <?php if(has_post_thumbnail()): ?>
@@ -106,12 +116,14 @@
                                             <p>
                                                 <a href="<?php echo $affiliateLink; ?>" class="btn btn-buy center-block"
                                                    role="button" rel="nofollow" target="_blank">
+                                                    <i class="fa fa-shopping-cart"></i>
                                                     <?php _e('Buy', 'affilicious-theme'); ?>
                                                 </a>
                                             </p>
                                         <?php endif; ?>
                                         <p>
                                             <a href="<?php the_permalink() ?>" class="btn btn-review center-block" role="button">
+                                                <i class="fa fa-book"></i>
                                                 <?php _e('To The Test Report', 'affilicious-theme'); ?>
                                             </a>
                                         </p>
